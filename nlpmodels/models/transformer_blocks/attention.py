7f386c2a0c8caf838a8cff1bce69f6d071ff79a4
will-thompson-k <19388895+will-thompson-k@users.noreply.github.com>
2020-10-26 07:20:34 -0500

    ** Added docstrings to attention.py

--- nlpmodels/models/transformer_blocks/attention.py
+++ nlpmodels/models/transformer_blocks/attention.py
@@ -12,15 +12,19 @@ class MultiHeadedAttention(nn.Module):
     and learns the relative importance of each to a target. Multi-headed means there
     are N of them working in parallel.
 
-    The particular attention function employed is “Scaled Dot-Product Attention" (scaled by 1/sqrt(d_k)).
+    This attention object is utilized in 2 ways:
+    (1) encoder-decoder attention (decoder[l-1]::queries, encoder::keys and values)
+    (2) self-attention (encoder[l-1]::queries,keys,and values)
+
+    The particular attention mechanism employed is “scaled dot-product attention" (scaled by 1/sqrt(d_k)).
     """
 
     def __init__(self, num_heads: int, dim_model: int, dropout: float):
         """
-           Args:
-               num_heads (int): number of heads/ simultanenous attention layers
-               dim_model (int): total concatenated size of attention layer output
-               dropout (float): dropout probability
+        Args:
+           num_heads (int): number of heads/ simultanenous attention layers
+           dim_model (int): total concatenated size of attention layer output + embedding input size
+           dropout (float): dropout probability
         """
         super(MultiHeadedAttention, self).__init__()
 
@@ -41,35 +45,41 @@ class MultiHeadedAttention(nn.Module):
 
     def compute_attention(self,query: torch.Tensor, key: torch.Tensor, value: torch.Tensor, mask: torch.Tensor):
         """
-        Computes attention math, returns attention matrix
+        Attention function to compute dot-product attention between query,
+        key and determine which values to "pay attention to".
+
+
         Args:
-            query (torch.Tensor): query matrix of (batch_size, num_heads,max_seq_length, dim_keys) size
-            key (torch.Tensor): key matrix of (batch_size, num_heads,max_seq_length, dim_keys) size
-            value (torch.Tensor): values matrix of (batch_size, num_heads,max_seq_length, dim_keys) size
-            mask (torch.Tensor): values matrix of (batch_size,1,1,max_seq_length) size
+            query (torch.Tensor): query 4D matrix of (batch_size, num_heads,max_seq_length, dim_keys) size
+            key (torch.Tensor): key 4D matrix of (batch_size, num_heads,max_seq_length, dim_keys) size
+            value (torch.Tensor): values 4D matrix of (batch_size, num_heads,max_seq_length, dim_keys) size
+            mask (torch.Tensor): values 4D matrix of (batch_size,1,1,max_seq_length) size
         Returns:
-            returns the attention matrix, attention probabilities (softmax output)
+            returns the attention values, attention probabilities (softmax output)
         """
+        # confirm that dim 4 is correct
         assert query.size(-1) == self._dim_keys
+
         # Scaled dot-product calculation (Q,K) -> scores
+        # NOTE: bmm is only for 3D tensors. matmul considers last 2 dimensions in batch matrix multiplication.
         scores = torch.matmul(query, key.transpose(2,3)) / math.sqrt(self._dim_keys)
-        # Wherever mask == False is where padding is, so ignore values
+        # Wherever mask == False is where padding is, so set scores to inf low for softmax calculation
         scores = scores.masked_fill(mask == False, -1e9)
         # Calculate soft-max of scores -> attention_probas (probabilities for each value)
         attention_probas = self._dropout(F.softmax(scores, dim=-1))
-        # attention is attention_probas * values
+        # attention is expected values = attention_probas * values
         attention_values = torch.matmul(attention_probas, value)
         return attention_values, attention_probas
 
     def forward(self, query, key, value, mask):
         """
         Main function call for attention mechanism.
-        maps (q,k,v) -> linear -> self_attn -> concat -> linear
+        maps (q,k,v) -> linear -> self_attn -> concat -> linear.
         Args:
             query (torch.Tensor): query matrix of (batch_size,_dim_keys) size
             key (torch.Tensor): key matrix of (batch_size,_dim_keys) size
             value (torch.Tensor): values matrix of (batch_size,_dim_values) size
-            mask (torch.Tensor): bool mask of (batch_size,1,max_seq_length) size
+            mask (torch.Tensor): bool mask 3D tensor of (batch_size,1,max_seq_length) size
         Returns:
             returns final linear layer output of (batch_size, max_seq_length, dim_model) size
         """
@@ -77,25 +87,23 @@ class MultiHeadedAttention(nn.Module):
         batch_size = query.size(0)
         max_seq_length = mask.size(-1)
 
-        # 1) Apply linear to query, key, and value, convert to 4D tensor
-        # (batch_size, num_heads,max_seq_length, dim_keys)
+        # 1) Apply linear to query, key, and value, convert to 4D matrix
+        # (batch_size,dim_keys) -> (batch_size, num_heads,max_seq_length, dim_keys)
         query = self._linear_layer_queries(query).view(batch_size, self._num_heads, max_seq_length, self._dim_keys)
         key = self._linear_layer_keys(key).view(batch_size, self._num_heads, max_seq_length, self._dim_keys)
         value = self._linear_layer_values(value).view(batch_size, self._num_heads, max_seq_length, self._dim_keys)
         # print(query.shape,self._num_heads,self._dim_keys,mask.shape)
 
-        # Same mask applied to all h heads.
-        # mask dims will become (batch_size,1,1,max_seq_length)
+        # (batch_size,1,max_seq_length) -> (batch_size,1,1,max_seq_length)
         mask = mask.unsqueeze(1)
 
-        # 2) compute attention
+        # 2) compute dot-product attention
         x, self._attention_tensor = self.compute_attention(query, key, value, mask)
 
-        # 3) concatenate
-        x = x.transpose(1, 2).contiguous().view(batch_size, max_seq_length, self._num_heads * self._dim_keys)
-        # x is now (batch_size, max_seq_length, dim_model)
+        # 3) "concatenate"/re-order from (batch_size,1,1,max_seq_length)->(batch_size, max_seq_length, dim_model)
+        x = x.transpose(1, 2).view(batch_size, max_seq_length, self._num_heads * self._dim_keys)
 
-        # 4) apply final linear
+        # 4) apply final linear layer
         x = self._linear_layer_final(x)
 
         return x
