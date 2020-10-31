d167c13baaba82269ac604b6037670e46c5e6413
will-thompson-k <19388895+will-thompson-k@users.noreply.github.com>
2020-10-30 23:01:16 -0500

    ** changed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='deeplearning-nlp-models',
     version='1.0',
     packages=['nlpmodels', 'nlpmodels.utils', 'nlpmodels.models', 'nlpmodels.models.transformer_blocks'],
-    setup_requires=['pytest-runner','torchtext==0.8.0a0+c851c3e','torch==1.6.0','numpy==1.19.1','datasets==1.1.2','tqdm==4.49.0'],
+    setup_requires=['pytest-runner','torchtext==0.8.0','torch==1.6.0','numpy==1.19.1','datasets==1.1.2','tqdm==4.49.0'],
     url='https://github.com/will-thompson-k/deeplearning-nlp-models',
     license='MIT',
     author='Will Thompson',
